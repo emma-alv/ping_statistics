bd53addef81a29b0836f749e8992551068500f77
Emmanuel Rodriguez <ing.erodrigueza@gmail.com>
2019-04-14 17:55:24 +0300

    Adding functions to filter information from ping files, using pandas dataframes to compute Average, Standard deviation and Variance. To plot values is necessary to install matplotlib

--- ping_statistics.py
+++ ping_statistics.py
@@ -1,20 +1,9 @@
 import os
 import pandas as pd
-
-listFiles = [i for i in os.listdir('.') if "google" in i]
-
-for i in listFiles:
-    with open(i) as data:
-        vars()["uc_"+i] = data.readlines()
-
-for i in listFiles:
-    vars()[i] = _clean_data(vars()["uc_"+i])
-
-for i in listFiles:
-    vars()[i] = _extend_data(vars()[i])
+import matplotlib.pyplot as plt
 
 
-def _clean_data(current_list):
+def _clean_data_(current_list):
 
     clean_data = []
     time = []
@@ -29,10 +18,37 @@ def _clean_data(current_list):
     return time
 
 
-def _extend_data(list_extend):
+def _extend_data_(list_extend):
 
     while len(list_extend) < 100:
         list_extend.append(0)
 
     return list_extend
+
+
+listFiles = [i for i in os.listdir('.') if "google" in i]
+d = {}
+
+for i in listFiles:
+    with open(i) as data:
+        vars()["uc_"+i] = data.readlines()
+
+for i in listFiles:
+    vars()[i] = _clean_data_(vars()["uc_"+i])
+
+for i in listFiles:
+    vars()[i] = _extend_data_(vars()[i])
+
+for i in listFiles:
+    d.update({i : vars()[i]})
+
+df = pd.DataFrame(d)
+
+df_means = pd.DataFrame(df.mean(axis=0))
+df_results = df_means.transpose()
+df_results = df_results.append(df.std(axis=0), ignore_index=True)
+df_results = df_results.append(df.var(axis=0), ignore_index=True)
+df_results.rename({0:'Average',1:'Standard deviation',2:'Variance'}, inplace=True)
+
+print(df_results)
 
--- ping_stats.sh
+++ ping_stats.sh
@@ -6,4 +6,5 @@ ping -c 100 -i 3 google.in > google_in
 ping -c 100 -i 3 google.mx > google_mx
 ping -c 100 -i 3 google.co.uk > google_uk
 
+python3 -mpip install matplotlib
 python3 ping_statistics.py
